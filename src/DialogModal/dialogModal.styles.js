4884bdcc61af9b5326d2565c5261e40ec4562d44
Marina Urbano Bouterin <m.urbano@englobatech.com>
2023-08-10 09:34:03 +0200

    Buttons fixed modal

--- src/DialogModal/dialogModal.styles.js
+++ src/DialogModal/dialogModal.styles.js
@@ -1,24 +1,25 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useDialogModalStyles = makeStyles((theme) => ({
-	dialogContent: {
-		overflow: 'initial',
-		padding: '24px',
-	},
-	actions: {
-		padding: '0 24px 24px 24px',
-	},
-	title: {
-		padding: '16px 24px 0 24px',
-	},
-	textButton: {
-		margin: 0,
-		marginRight: '0.2rem',
-		marginTop: '0.1rem',
-		marginLeft: '0.2rem',
-	},
-	wrapperButton: {
-		display: 'flex',
-		alignItems: 'center',
-	},
+export const useDialogModalStyles = makeStyles(theme => ({
+  dialogContent: {
+    overflow: 'auto',
+    padding: '24px',
+    overflowX: 'hidden'
+  },
+  actions: {
+    padding: '0 24px 24px 24px'
+  },
+  title: {
+    padding: '16px 24px 0 24px'
+  },
+  textButton: {
+    margin: 0,
+    marginRight: '0.2rem',
+    marginTop: '0.1rem',
+    marginLeft: '0.2rem'
+  },
+  wrapperButton: {
+    display: 'flex',
+    alignItems: 'center'
+  }
 }));
