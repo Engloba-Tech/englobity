e81323a0571a469d039ff1aed9f2d4db532b3cf5
Marina Urbano Bouterin <m.urbano@englobatech.com>
2023-10-31 17:05:55 +0100

    numeric value select

--- src/Select/Select.js
+++ src/Select/Select.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import shortid from 'shortid';
-import { InputLabel, MenuItem, FormControl, Select, ListItemIcon, FormHelperText } from '@material-ui/core';
+import { FormControl, FormHelperText, InputLabel, ListItemIcon, MenuItem, Select } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import PropTypes from 'prop-types';
+import React from 'react';
+import shortid from 'shortid';
 import { useCustomSelectStyles } from './select.styles';
 
 export function CustomSelect({
@@ -18,11 +18,14 @@ export function CustomSelect({
   errorMessage,
   submitted,
   error,
+  numericValue = false,
   ...props
 }) {
   const id = shortid.generate();
   const classes = useCustomSelectStyles();
-  const isRequiredError = (required && submitted && !value) || error;
+  const isRequiredError = numericValue
+    ? required && submitted && isNaN(value)
+    : (required && submitted && !value) || error;
   return (
     <FormControl error={error} {...props} variant="outlined" className={`${classes.root} ${className}`}>
       {isLoading ? (
@@ -82,5 +85,6 @@ CustomSelect.propTypes = {
   required: PropTypes.bool,
   errorMessage: PropTypes.string,
   error: PropTypes.bool,
-  submitted: PropTypes.bool
+  submitted: PropTypes.bool,
+  numericValue: PropTypes.bool
 };
